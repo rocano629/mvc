b1c695d11e15908b5ef09f34273221c12385560b
rcano <rcano@RCANOVMTEST>
2016-11-15 12:31:16 -0600

    Add project files.

---
+++ PartyInvites2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites2.Models;
+
+namespace PartyInvites2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+        
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Rsvpform(GuestResponse guestResponse) 
+        { 
+            //TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
+        }
+      
+	}
+}
